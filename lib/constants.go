761565aa2c7259c29756d9eb87cefa259adb65f8
sontv <sontv@vnext.vn>
2026-05-25 16:21:51 +0700

    update constanta

--- lib/constants.go
+++ lib/constants.go
@@ -1,18 +1,11 @@
 package main
 
 const (
-	// broker    = "test.rmcs.d6-vnext.com"
-	// port      = 1883
-	// username  = "vnext-test_b6239876-943a-4d6f-a7ef-f1440d5c58af"
-	// password  = "7#TlDprf"
-	// thingName = "vnext-test_b6239876-943a-4d6f-a7ef-f1440d5c58af"
-	// clientID  = "go-backend-rmcs-client"
-	// baseTopic = "vnext-test_b6239876-943a-4d6f-a7ef-f1440d5c58af/robot-control"
-	broker    = "rmcs.d6-vnext.com"
+	broker    = "test.rmcs.d6-vnext.com"
 	port      = 1883
-	username  = "0f5c126f-e339-4f99-a8a0-4b4fe29944f0"
-	password  = "!1l!YC1#"
-	thingName = "0f5c126f-e339-4f99-a8a0-4b4fe29944f0"
+	username  = "vnext-test_b6239876-943a-4d6f-a7ef-f1440d5c58af"
+	password  = "7#TlDprf"
+	thingName = "vnext-test_b6239876-943a-4d6f-a7ef-f1440d5c58af"
 	clientID  = "go-backend-rmcs-client"
-	baseTopic = "0f5c126f-e339-4f99-a8a0-4b4fe29944f0/robot-control"
+	baseTopic = "vnext-test_b6239876-943a-4d6f-a7ef-f1440d5c58af/robot-control"
 )
